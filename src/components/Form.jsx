3f502eb03fe9c1231602b1c68b8d6f7d636f4a20
Dmitriy Sakaluk <78900270+owin66@users.noreply.github.com>
2023-04-17 21:23:16 +0300

    add handle to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 
 export default class Form extends Component {
   state = {
-    name: '',
-    text: '',
+    inputText: '',
+    textareaText: '',
+  };
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({ inputText: value });
+  };
+
+  handleTextareaChange = ({ target: { value } }) => {
+    this.setState({ textareaText: value });
   };
 
   render() {
+    const { inputText, textareaText } = this.state;
     return (
       <>
         <form action="">
           {/* input  */}
           <label htmlFor="">
             Name:
-            <input type="text" name="name" />
+            <input
+              type="text"
+              name="name"
+              value={inputText}
+              onChange={this.handleInputChange}
+            />
           </label>
           <br />
           {/* textarea */}
           <label htmlFor="">Text:</label>
-          <textarea name="text" />
+          <textarea
+            name="text"
+            value={textareaText}
+            onChange={this.handleTextareaChange}
+          />
           <br />
           {/* button */}
           <button>Show</button>
