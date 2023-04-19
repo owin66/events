f6abea4ee429ae1291e9a5e6e42ea9ac1a496b77
Dmitriy Sakaluk <78900270+owin66@users.noreply.github.com>
2023-04-19 08:15:06 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,15 @@
-import { useState } from 'react';
-import Rendering from './Rendering';
 import Table from './Table';
-import Form from './Form';
 import Refs from './Refs';
+import HOC from './HOC';
+import { LessonOne } from './LessonOne';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const people = ['Jack', 'Bayonet', 'Kostya', 'Yana', 'Pasha'];
-
-  const TABS_BTN = [
-    { dataName: 1, title: 'Tab1', icon: '+' },
-    { dataName: 2, title: 'Tab2', icon: '*' },
-    { dataName: 3, title: 'Tab3', icon: '$' },
-  ];
-
   return (
     <>
-      <h1>HI {count >= 3 ? `this count = ${count}` : 'dima'}</h1>
-      <h2>{count}</h2>
-      <button type="button" onClick={() => increment()}>
-        +1
-      </button>
-      <button type="button" onClick={() => decrement()}>
-        -1
-      </button>
-      <br />
-      {count >= 1 ? <Rendering peoples={people} tabs={TABS_BTN} /> : null}
+      <LessonOne />
       <Table />
       <Refs />
+      <HOC />
     </>
   );
 };
---
+++ src/components/LessonOne.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Rendering from './Rendering';
+
+export const LessonOne = () => {
+  const people = ['Jack', 'Bayonet', 'Kostya', 'Yana', 'Pasha'];
+
+  const TABS_BTN = [
+    { dataName: 1, title: 'Tab1', icon: '+' },
+    { dataName: 2, title: 'Tab2', icon: '*' },
+    { dataName: 3, title: 'Tab3', icon: '$' },
+  ];
+
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <h1>HI {count >= 3 ? `this count = ${count}` : 'dima'}</h1>
+      <h2>{count}</h2>
+      <button type="button" onClick={() => increment()}>
+        +1
+      </button>
+      <button type="button" onClick={() => decrement()}>
+        -1
+      </button>
+      <br />
+      {count >= 1 ? <Rendering peoples={people} tabs={TABS_BTN} /> : null}
+    </>
+  );
+};
