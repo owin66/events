3b0c2b3efb2bf9751ad15ddb531bd28781c4a0d9
Dmitriy Sakaluk <78900270+owin66@users.noreply.github.com>
2023-04-21 14:52:23 +0300

    add Fragment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,15 @@ import Table from './Table';
 import Refs from './Refs';
 import HOC from './HOC';
 import { LessonOne } from './LessonOne';
+import { Fragment } from 'react';
 
 export const App = () => {
   return (
-    <>
+    <Fragment>
       <LessonOne />
       <Table />
       <Refs />
       <HOC />
-    </>
+    </Fragment>
   );
 };
